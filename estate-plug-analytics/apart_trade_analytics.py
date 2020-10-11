cc6d20a14720652a2fc836b1b4937019d3497aba
jj-pa <krap9482@gmail.com>
2020-10-11 15:38:11 +0900

    Add an apartment analytics source

--- estate-plug-analytics/apart_trade_analytics.py
+++ estate-plug-analytics/apart_trade_analytics.py
@@ -1,58 +1,60 @@
 import pandas as pd
-import json
 from datetime import date
 from dateutil.relativedelta import relativedelta
-from io import StringIO # Python 3.x
 from mongodb import connect_mongo, insert_apart_trade_targetarea_increase
 from s3 import get_s3_client, print_buckets, print_bucket_files
 from util import count_trade, read_trade, target_area_trade_increase_decrease, per_delta
-#%%
+
+# %%
 client = get_s3_client()
-#%%
+# %%
 print_buckets(client)
-#%%
+# %%
 print_bucket_files(client, 'store-estate')
-#%%
-# 15년 전월 대비 거래량 차이
-client = get_s3_client()
+# %%
+# 전월 대비 거래량 차이 (연 매매 증감율) - 2015, 2016
+client = get_s3_client()  # s3 client
+
 # 날짜 변수 생성
 year_month_data = list()
-start_year = 2015
+start_year = 2016
 start_month = 1
-end_year = 2015
+end_year = 2016
 end_month = 12
+
 # 날짜 범위만큼 아파트 매매 데이터 로드
 df_trade_count_arr = list()
 for result in per_delta(date(year=int(start_year), month=int(start_month), day=1),
                         date(year=int(end_year), month=int(end_month), day=28),
                         relativedelta(months=1)):
-    year_month_data.append(result.strftime("%Y%m")) # 날짜 배열에 저장
+    year_month_data.append(result.strftime("%Y%m"))  # 연월 관리 변수에 추가
     df_trade = read_trade(_s3Obj=client, _item="apart", _year=result.strftime("%Y"), _month=result.strftime("%m"))
-    df_trade_count = count_trade(df_trade)
-    df_trade_count_arr.append(df_trade_count)
+    df_trade_count = count_trade(df_trade)  # 아파트 매매 거래 한달 데이터 -> 지역코드별 카운트 산출
+    df_trade_count_arr.append(df_trade_count)  # 지역코드별 로우 개수 List 추가
 
 # 전월 대비 거래량 차이 저장
 diff_prev_month = list()
-result_df = pd.DataFrame(columns=['area_code', 'year_month', 'value'])
+result_df = pd.DataFrame(columns=['area_code', 'year_month', 'value'])  # 결과 프레임 생성 (지역코드, 연월, 카운트)
 for idx, item in enumerate(df_trade_count_arr):
     # 처음 루틴에서는 처음 월을 위해 0 값 설정 데이터 생성
     if idx == 0:
         init_df = df_trade_count_arr[idx + 1] - df_trade_count_arr[idx]
-        init_df['year_month'] = year_month_data[0]  # 초기 날짜 데이터 추가 (201501)
+        init_df['year_month'] = year_month_data[0]  # 초기 날짜 데이터 추가 (ex: 201501)
         init_df['area_code'] = init_df.index  # 지역코드 컬럼 추가 (인덱스)
         init_df.reset_index(level=0, inplace=True, drop=True)  # 인덱스 초기화
         init_df.rename(columns={'지역코드': 'value'}, inplace=True)  # 값 컬럼명 변경
         init_df['value'] = 0  # 초기값 0으로 설정
         init_df = init_df[['area_code', 'year_month', 'value']].sort_values(by=['area_code'], axis=0)  # 컬럼 순서 변경
         result_df = pd.concat([result_df, init_df], ignore_index=True)
+
     # 결과 데이터프레임에 전월 대비 프레임 병합
     if idx < len(df_trade_count_arr) - 1:
         diff_df = df_trade_count_arr[idx + 1] - df_trade_count_arr[idx]
-        diff_df['year_month'] = year_month_data[idx + 1] # 날짜 데이터 추가
-        diff_df['area_code'] = diff_df.index # 지역코드 컬럼 추가 (인덱스)
-        diff_df.reset_index(level=0, inplace=True, drop=True) # 인덱스 초기화
-        diff_df.rename(columns={'지역코드': 'value'}, inplace=True) # 값 컬럼명 변경
-        diff_df = diff_df[['area_code', 'year_month', 'value']].sort_values(by=['area_code'], axis=0) # 컬럼 순서 변경
+        diff_df['year_month'] = year_month_data[idx + 1]  # 날짜 데이터 추가
+        diff_df['area_code'] = diff_df.index  # 지역코드 컬럼 추가 (인덱스)
+        diff_df.reset_index(level=0, inplace=True, drop=True)  # 인덱스 초기화
+        diff_df.rename(columns={'지역코드': 'value'}, inplace=True)  # 값 컬럼명 변경
+        diff_df = diff_df[['area_code', 'year_month', 'value']].sort_values(by=['area_code'], axis=0)  # 컬럼 순서 변경
         result_df = pd.concat([result_df, diff_df], ignore_index=True)
 
 print(result_df)
@@ -62,15 +64,17 @@ database = connect_mongo()
 collection = database["apart_trade_monthly_increase_decrease"]
 collection.insert_many(result_df.to_dict('records'))
 
-#%%
-# 15년 지역별 월 평균 거래 금액
-client = get_s3_client()
+# %%
+# 지역별 월 평균 거래 금액 - 2015, 2016
+client = get_s3_client()  # s3 client
+
 # 날짜 변수 생성
 year_month_data = list()
-start_year = 2015
+start_year = 2016
 start_month = 1
-end_year = 2015
+end_year = 2016
 end_month = 12
+
 # 날짜 범위만큼 아파트 매매 데이터 로드
 result_df = pd.DataFrame(columns=['area_code', 'price'])
 for result in per_delta(date(year=int(start_year), month=int(start_month), day=1),
@@ -79,11 +83,12 @@ for result in per_delta(date(year=int(start_year), month=int(start_month), day=1
     df_trade = read_trade(_s3Obj=client, _item="apart", _year=result.strftime("%Y"), _month=result.strftime("%m"))
     df_trade.drop(columns=['지번', '층', '년', '월', '일', '아파트', '법정동', '매매일', '건축년도', '전용면적'], inplace=True)
     df_trade.rename(columns={'거래금액': 'price', '지역코드': 'area_code'}, inplace=True)  # 값 컬럼명 변경
-    # df_trade['year_month'] = result.strftime("%Y%m")  # 날짜 데이터 추가
+    df_trade['year_month'] = result.strftime("%Y%m")  # 날짜 데이터 추가
     df_trade = df_trade[df_trade['price'].astype(str) != '거래금액'].astype(float)
     df_trade.astype({"area_code": 'str'})
     df_trade.astype({"price": 'float'})
-    df_trade = df_trade.groupby(by=['area_code'], as_index=False).mean()
+
+    df_trade = df_trade.groupby(by=['area_code', 'year_month'], as_index=False).mean()  # 지역 그룹
     result_df = pd.concat([result_df, df_trade], ignore_index=True)
 
 print(result_df)
@@ -92,3 +97,72 @@ print(result_df)
 database = connect_mongo()
 collection = database["apart_trade_monthly_average_price"]
 collection.insert_many(result_df.to_dict('records'))
+
+# %%
+# 연 매매 거래비중 상위 10개 구 (거래금액) - 2015, 2016
+client = get_s3_client()  # s3 client
+
+# 날짜 변수 생성
+year_month_data = list()
+start_year = 2016
+start_month = 1
+end_year = 2016
+end_month = 12
+
+# 날짜 범위만큼 아파트 매매 데이터 로드
+result_df = pd.DataFrame(columns=['area_code', 'price'])
+for result in per_delta(date(year=int(start_year), month=int(start_month), day=1),
+                        date(year=int(end_year), month=int(end_month), day=28),
+                        relativedelta(months=1)):
+    df_trade = read_trade(_s3Obj=client, _item="apart", _year=result.strftime("%Y"), _month=result.strftime("%m"))
+    df_trade.drop(columns=['지번', '층', '년', '월', '일', '아파트', '법정동', '매매일', '건축년도', '전용면적'], inplace=True)
+    df_trade.rename(columns={'거래금액': 'price', '지역코드': 'area_code'}, inplace=True)  # 값 컬럼명 변경
+    df_trade['year'] = result.strftime("%Y")  # 날짜 데이터 추가
+    df_trade = df_trade[df_trade['price'].astype(str) != '거래금액'].astype(float)  # 이상 값 필터링
+    df_trade.astype({"area_code": 'str'})
+    df_trade.astype({"price": 'float'})
+
+    df_trade = df_trade.groupby(by=['area_code', 'year'], as_index=False).sum()
+    result_df = pd.concat([result_df, df_trade], ignore_index=True)
+
+result_df = result_df.groupby(by=['area_code', 'year'], as_index=False).sum().\
+    sort_values(by='price', ascending=False).head(10)
+
+# 데이터베이스 프레임 삽입
+database = connect_mongo()
+collection = database["apart_trade_yearly_sum_price_top_10"]
+collection.insert_many(result_df.to_dict('records'))
+
+
+# %%
+# 연 매매 거래비중 상위 10개 구 (매매 건) - 2015, 2016
+client = get_s3_client()  # s3 client
+
+# 날짜 변수 생성
+year_month_data = list()
+start_year = 2016
+start_month = 1
+end_year = 2016
+end_month = 12
+
+# 날짜 범위만큼 아파트 매매 데이터 로드
+result_df = pd.DataFrame(columns=['area_code', 'count'])
+for result in per_delta(date(year=int(start_year), month=int(start_month), day=1),
+                        date(year=int(end_year), month=int(end_month), day=28),
+                        relativedelta(months=1)):
+    df_trade = read_trade(_s3Obj=client, _item="apart", _year=result.strftime("%Y"), _month=result.strftime("%m"))
+    df_trade_count = count_trade(df_trade)  # 아파트 매매 거래 한달 데이터 -> 지역코드별 카운트 산출
+    df_trade_count['year'] = result.strftime("%Y")  # 날짜 데이터 추가
+    df_trade_count['area_code'] = df_trade_count.index  # 지역코드 컬럼 추가 (인덱스)
+    df_trade_count.reset_index(level=0, inplace=True, drop=True)  # 인덱스 초기화
+    df_trade_count.rename(columns={'지역코드': 'count'}, inplace=True)  # 값 컬럼명 변경
+    result_df = pd.concat([result_df, df_trade_count], ignore_index=True)
+
+result_df = result_df.groupby(by=['area_code', 'year'], as_index=False).sum().\
+    sort_values(by='count', ascending=False).head(10)
+
+# 데이터베이스 프레임 삽입
+database = connect_mongo()
+collection = database["apart_trade_yearly_count_top_10"]
+collection.insert_many(result_df.to_dict('records'))
+
--- estate-plug-analytics/mongodb/MongoClient.py
+++ estate-plug-analytics/mongodb/MongoClient.py
@@ -3,18 +3,18 @@ from pymongo import MongoClient  # for MongoDB
 
 """
 *******************************************************************************
-* Connect_Mongo()                     MongDB 접속
-* @return                 <mydb>      MogoDB 반환
+* Connect_Mongo()                     MongoDB 접속
+* @return                 <database>  MongoDB 반환
 *******************************************************************************
 """
 def connect_mongo():
     my_client = MongoClient(
         "mongodb://root:P%40ssw0rd@52.79.55.128:27017/?authSource=admin&readPreference=primary&ssl=false")
-
+    # my_client = MongoClient(
+    #     "mongodb://jj-pa:2020#JJ-pa@192.168.219.111:27017/?authSource=admin&readPreference=primary&ssl=false")
     print(my_client.list_database_names())
-
-    mydb = my_client['estate-plug']
-    return mydb
+    database = my_client['estate']
+    return database
 
 
 def insert_apart_trade_targetarea_increase(target_area_codeData, year_monthData, increaseData):
